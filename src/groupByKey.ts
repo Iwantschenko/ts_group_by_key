fe4c1894f10b89ea32b9a078ff6341775cc86051
Iwantschenko <160021860+Iwantschenko@users.noreply.github.com>
2025-03-14 16:58:56 +0200

    done task

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,17 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const resObj: GroupsMap<T> = {};
+
+  for (const item of items) {
+    if (!resObj[item[key] as string]) {
+      resObj[item[key] as string] = [];
+    }
+
+    resObj[item[key] as string].push(item);
+  }
+
+  return resObj;
 }
